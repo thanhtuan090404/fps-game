9983336a94a9c0835a112e60da72426dfa2a1f4d
thanhtuan090404 <ntuan090404@gmail.com>
2026-07-13 05:41:13 +0700

    chore: initial FPS game project setup

---
+++ Assets/script/FPSMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class FPSMovement : MonoBehaviour
+{
+    private CharacterController controller; // biến lưu trữ tham chiếu đến CharacterController của người chơi
+    [SerializeField] private float speed = 5f;
+    private float h, v; // biến lưu trữ giá trị input từ bàn phím
+    private float gravity = -9.81f; // giá trị trọng lực
+    private float velocityY; // biến lưu trữ vận tốc rơi của người chơi
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = GetComponent<CharacterController>(); // lấy tham chiếu đến CharacterController
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool grounded = controller.isGrounded; // kiểm tra xem người chơi có đang đứng trên mặt đất hay không
+        if (grounded && velocityY < 0)
+        {
+            velocityY = -2f;
+        }
+        h = Input.GetAxis("Horizontal"); // lấy giá trị input từ bàn phím (A,D)
+        v = Input.GetAxis("Vertical"); // lấy giá trị input từ bàn phím (W,S)
+        Vector3 movement = transform.right * h + transform.forward * v; // tính toán hướng di chuyển dựa trên hướng camera
+        controller.Move(movement * speed * Time.deltaTime); // di chuyển người chơi theo hướng camera
+        velocityY += gravity * Time.deltaTime; // tính toán vận tốc rơi
+        controller.Move(new Vector3(0, velocityY, 0) * Time.deltaTime); // di chuyển người chơi theo trọng lực
+    }
+}
---
+++ Assets/script/MouseLook.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] private Transform cameraTransform;
+
+    private float _xRotation = 0f; // biến lưu trữ góc xoay dọc của camera
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // khóa và ẩn con trỏ chuột
+        Cursor.lockState = CursorLockMode.Locked; // khóa chuột vô giữu
+        Cursor.visible = false; // ẩn con trỏ chuột
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        // xoay thân người ngang
+        transform.Rotate(Vector3.up * mouseX); // xoay quanh trục y
+
+        // ngẩng cúi camera (dọc ) cần CLAMP
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f); // giới hạn góc nhìn lên xuống
+        cameraTransform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f); // xoay camera quanh trục x
+
+    }
+}
