e8835ed164f5238241f099ba899c140a2f030c10
thanhtuan090404 <ntuan090404@gmail.com>
2026-07-13 10:16:44 +0700

    feat: add raycast shooting system with damage

---
+++ Assets/script/ngay16_Raycast/Gun.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    private float damage = 20f;
+    private float range = 100f;
+    [SerializeField] public Camera playerCamera;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+            Debug.DrawRay(playerCamera.transform.position, playerCamera.transform.forward * range, Color.red, 1f);
+
+        }
+    }
+
+    private void Shoot()
+    {
+        if(Physics.Raycast(playerCamera.transform.position,playerCamera.transform.forward, out RaycastHit hit, range))
+        {
+            Debug.Log("trung :" + hit.transform.name);
+            // kiểm tra xem có phải enemy không , nếu phải thi sẽ gây damage
+            Target target = hit.transform.GetComponent<Target>();  // lấy component Target của đối tượng bị trúng đạn
+            if (target != null) // nếu có component Target thì gây damage
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
---
+++ Assets/script/ngay16_Raycast/Target.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField] private float health = 50f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        Debug.Log(gameObject.name + " còn " + health + " HP");
+
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log(gameObject.name + " bị tiêu diệt!");
+        Destroy(gameObject);
+    }
+}
